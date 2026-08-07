f7864317812e277d4789710c97cf21845354fce6
Vignesh Manoharan <86256512+Vignesh2308m@users.noreply.github.com>
2026-08-07 22:56:18 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn encode(strs: Vec<String>) -> String {
+        strs.join(" ")
+    }
+
+    pub fn decode(s: String) -> Vec<String> {
+        s.split(" ").map(|s| s.to_string()).collect()
+    }
+}
